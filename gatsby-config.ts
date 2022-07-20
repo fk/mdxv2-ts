cb4fd9bca40a1ea62068de3ca3f4ee59e8f4b987
Florian Kissling <florian.kissling@gmail.com>
2022-07-20 23:10:36 +0200

    Allthethings

---
+++ gatsby-config.ts
@@ -0,0 +1,26 @@
+import type { GatsbyConfig } from "gatsby"
+import { remarkGfm } from "remark-gfm"
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-mdx",
+      mdxOptions: {
+        remarkPlugins: [remarkGfm],
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+}
+
+export default config
